d1f1dcd5112a2d7422615d6699568841d2f67868
KrisnaCahya <krisnacahyap2@gmail.com>
2022-04-02 09:39:36 +0700

    add login page

---
+++ lib/halaman_login.dart
@@ -0,0 +1,123 @@
+import 'package:pbma7/style/style.dart';
+import 'package:pbma7/halaman_daftar.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 75, 203, 220),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('LOGIN',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+              Text(
+                'Login To Your Account',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15, bottom: 15),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(90, 108, 234, 0.07),
+                      blurRadius: 50,
+                      spreadRadius: 0,
+                      offset: Offset(12, 26))
+                ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: borderRadius1,
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'example@gmail.com',
+                    labelText: 'Email',
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: color1,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15, bottom: 15),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(90, 108, 234, 0.07),
+                      blurRadius: 50,
+                      spreadRadius: 0,
+                      offset: Offset(12, 26))
+                ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: borderRadius1,
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'password123',
+                    labelText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: color1,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 7, horizontal: 20),
+                decoration: BoxDecoration(
+                    borderRadius: borderRadius1,
+                    color: color1,
+                    boxShadow: [boxshadow1]),
+                child: TextButton(
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {}),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) {
+                          return register();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'create account',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
